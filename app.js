79a4b92dcb19df0ca7c6ed62e105e200097c284c
Xao20 <103566148+Xao20@users.noreply.github.com>
2023-06-10 16:30:32 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const main = document.querySelector('.header .nav-bar .nav-list .main');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+main.addEventListener('click', () => {
+	main.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#000000';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		main.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
